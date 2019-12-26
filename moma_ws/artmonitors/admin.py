ce5d41ed4ef9e164cbb1e41fe9c316c7245cbd35
ljacobo@ncsu.edu <ljacobo@ncsu.edu>
2019-12-26 15:30:20 -0500

    Add AdminModel for TemplateCollection to ignore json_data

--- moma_ws/artmonitors/admin.py
+++ moma_ws/artmonitors/admin.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 
 from .models import Collection, TemplateCollection, Work
 
+
+class TemplateCollectionAdmin(admin.ModelAdmin):
+    exclude = ('json_data',)
+
+
 admin.site.register(Collection)
-admin.site.register(TemplateCollection)
+admin.site.register(TemplateCollection, TemplateCollectionAdmin)
 admin.site.register(Work)
