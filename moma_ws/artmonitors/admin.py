83c4c280427bfbd7e6128b3c39c8b7e3d4b51ad3
ljacobo@ncsu.edu <ljacobo@ncsu.edu>
2019-12-26 17:11:42 -0500

    move get_queryset() to Admin class

--- moma_ws/artmonitors/admin.py
+++ moma_ws/artmonitors/admin.py
@@ -8,6 +8,13 @@ from .models import Collection, TemplateCollection, Work
 class TemplateCollectionAdmin(admin.ModelAdmin):
     exclude = ('json_data',)
 
+    def get_queryset(self, request):
+        # https://stackoverflow.com/questions/34774028/how-to-ignore-loading-huge-fields-in-django-admin-list-display
+        qs = super(TemplateCollectionAdmin, self).get_queryset(request)
+        # tell Django to not retrieve json_data field from DB
+        qs = qs.defer('json_data')
+        return qs
+
 
 admin.site.register(Collection)
 admin.site.register(TemplateCollection, TemplateCollectionAdmin)
